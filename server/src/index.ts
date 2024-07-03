fe9f5b276efa85e2ff29aab38b428a80b1019d74
Sachin Tom <sachin.tom.dev@gmail.com>
2024-07-03 11:04:13 +0530

    chore: updated gitignore and server port

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
   res.json({});
 });
 
-app.listen(3000, () => {
-  console.log(`listening on 3000 ✅`);
+app.listen(4000, () => {
+  console.log(`listening on 4000 ✅`);
 });
